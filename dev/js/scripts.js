a7bf2d6e2a25327f207840f92ae82622f6c88dcf
yukiiimak <yukimak821@gmail.com>
2021-06-03 18:57:15 -0400

    added javascript code

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,16 @@
-$("body").css("background", "none");
+import $ from "jquery";
+
+let $nav = $('nav[data-nav]');
+let isVisible = false;
+
+$(".nav-btns").on("click", function(){
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+})
